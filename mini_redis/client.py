a95517b1f8a6800641ce4253959d92effbd0be0b
Your Name <you@example.com>
2023-11-09 05:26:47 +0000

    added some more functions

--- mini_redis/client.py
+++ mini_redis/client.py
@@ -1,4 +1,6 @@
 import requests
+import json
+
 
 class MiniRedisClient:
     def __init__(self, server_url: str):
@@ -7,12 +9,19 @@ class MiniRedisClient:
     def __getitem__(self, key: str):
         response = requests.get(f"{self.server_url}/get/{key}")
         if response.status_code == 200:
-            return response.json()['value']
+            return json.loads(response.json()['value'])
         else:
             raise KeyError('Key not found')
+        
+    def get(self, key: str, default=None):
+        response = requests.get(f"{self.server_url}/get/{key}")
+        if response.status_code == 200:
+            return json.loads(response.json()['value'])
+        else:
+            raise default
 
     def __setitem__(self, key: str, value):
-        response = requests.post(f"{self.server_url}/set/{key}", data=value)
+        response = requests.post(f"{self.server_url}/set/{key}", data=json.dumps(value))
         if response.status_code != 200:
             raise Exception('Failed to set value')
 
@@ -31,11 +40,11 @@ class MiniRedisClient:
 
     def values(self):
         response = requests.get(f"{self.server_url}/values")
-        return response.json()['values']
+        return [json.loads(x) for x in response.json()['values']]
 
     def items(self):
         response = requests.get(f"{self.server_url}/items")
-        return response.json()['items']
+        return {k:json.loads(v) for k,v in response.json()['items']}
 
     def clear(self):
         response = requests.delete(f"{self.server_url}/clear")
