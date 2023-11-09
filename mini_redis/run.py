bb79cbfd01eccf0d20fb5d431dec60f4c6be2461
Your Name <you@example.com>
2023-11-09 06:59:46 +0000

    ..

---
+++ mini_redis/run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from uvicorn import run
+import fire
+# run.py
+def run_server(host:str="0.0.0.0",port:int=8000,reload:bool=True):
+    run("mini_redis.main:app", host=host, port=port, reload=reload)
+
+    
+def main():
+    fire.Fire(run_server)
+
+if __name__ == '__main__':
+    main()
