6cd8e21074f9dfe8ecdba363097aa9932a72f06b
Your Name <you@example.com>
2023-11-13 01:05:47 +0000

    added error handeling

--- mini_redis/main.py
+++ mini_redis/main.py
@@ -22,58 +22,84 @@ async def set_key(key: str, request: Request):
     body = await request.body()
     # print(key,value)
     # Store the value at the given key
-    with lock, db:
-        db[key] = body
-    return {"success": True}
+    try:
+        with lock, db:
+            db[key] = body
+        return {"success": True}
+    except Exception:
+        return {"error": "Failed to set key"}
 
 
 @app.get("/get/{key}")
 def get_key(key: str) -> Any:
     # Retrieve the value at the given key
-    with lock, db:
-        value = db.get(key, None)
-        if value is not None:
-            return {"value": value}
-    return {"error": "Key not found"}
+    try:
+        with lock, db:
+            value = db.get(key, None)
+            if value is not None:
+                return {"value": value}
+    except KeyError:
+        return {"error": "Key not found"}
+    except Exception:
+        return {"error": "Failed to get key"}
 
 
 @app.delete("/delete/{key}")
 def delete_key(key: str):
     # Remove the value at the given key
-    with lock, db:
-        if key in db:
-            del db[key]
-            return {"success": True}
-    return {"error": "Key not found"}
+    try:
+        with lock, db:
+            if key in db:
+                del db[key]
+                return {"success": True}
+    except KeyError:
+        return {"error": "Key not found"}
+    except Exception:
+        return {"error": "Failed to delete key"}
 
 
 @app.get("/exists/{key}")
 def key_exists(key: str):
     # Check if the key exists in the database
-    with lock, db:
-        return {"exists": key in db}
+    try:
+        with lock, db:
+            return {"exists": key in db}
+    except Exception:
+        return {"error": "Failed to check if key exists"}
 
 @app.get("/keys")
 def get_keys():
     # Return all keys in the database
-    with lock, db:
-        return {"keys": list(db.keys())}
+    try:
+        with lock, db:
+            return {"keys": list(db.keys())}
+    except:
+        return {"error": "Failed to get keys"}
 
 @app.get("/values")
 def get_values():
     # Return all values in the database
-    with lock, db:
-        return {"values": list(db.values())}
+    try:
+        with lock, db:
+            return {"values": list(db.values())}
+    except:
+        return {"error": "Failed to get values"}
 
 @app.get("/items")
 def get_items():
     # Return all items in the database
-    with lock, db:
-        return {"items": list(db.items())}
+    try:
+        with lock, db:
+            return {"items": list(db.items())}
+    except:
+        return {"error": "Failed to get items"}
 
 @app.delete("/clear")
 def clear_db():
     # Clear the database
-    with lock, db:
-        db.clear()
-        return {"success": True}
+    try:
+        with lock, db:
+            db.clear()
+            return {"success": True}
+    except:
+        return {"error": "Failed to clear database"}
